4c72dc4baa2937e67048cb505e3a4cee4a1f4499
shajidrahimclass-jpg <shajidrahimclass@gmail.com>
2026-05-29 23:34:31 +0600

    chore: update dependencies and configure SEO provider
    Add `react-helmet-async` for SEO management and update build configuration, Tailwind plugins, and core dependencies to align with modern build requirements.

--- src/App.tsx
+++ src/App.tsx
@@ -11,29 +11,32 @@ import { WishlistProvider } from './contexts/WishlistContext';
 import getRoutes from './routes';
 import { Toaster } from './components/ui/sonner';
 import { ThemeProvider } from 'next-themes';
+import { HelmetProvider } from 'react-helmet-async';
 
 export default function App() {
   const routes = getRoutes();
 
   return (
-    <BrowserRouter>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <AuthProvider>
-          <AppSettingsProvider>
-            <KeyboardShortcutsProvider>
-              <WishlistProvider>
-                <Routes>
-                  {routes.map((route) => (
-                    <Route key={route.path} path={route.path} element={route.element} />
-                  ))}
-                </Routes>
-                <Toaster />
-              </WishlistProvider>
-            </KeyboardShortcutsProvider>
-          </AppSettingsProvider>
-        </AuthProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <AuthProvider>
+            <AppSettingsProvider>
+              <KeyboardShortcutsProvider>
+                <WishlistProvider>
+                  <Routes>
+                    {routes.map((route) => (
+                      <Route key={route.path} path={route.path} element={route.element} />
+                    ))}
+                  </Routes>
+                  <Toaster />
+                </WishlistProvider>
+              </KeyboardShortcutsProvider>
+            </AppSettingsProvider>
+          </AuthProvider>
+        </ThemeProvider>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
